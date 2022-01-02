8aefc55ee1bc2f25d611bd7e295475ce31b774d7
Charles Quesnel <quesnel.charles@gmail.com>
2022-01-02 09:18:22 -0500

    Change gnome extensions

--- scripts/gnome_extensions.sh
+++ scripts/gnome_extensions.sh
@@ -7,11 +7,12 @@
 # Read the included LICENSE file for more information
 
 (xdg-open "https://extensions.gnome.org/extension/906/sound-output-device-chooser/" &>/dev/null) &
-(xdg-open "https://extensions.gnome.org/extension/779/clipboard-indicator/" &>/dev/null) &
+(xdg-open "https://extensions.gnome.org/extension/1160/dash-to-panel/" &>/dev/null) &
+(xdg-open "https://extensions.gnome.org/extension/3628/arcmenu/" &>/dev/null) &
 (xdg-open "https://extensions.gnome.org/extension/945/cpu-power-manager/" &>/dev/null) &
 (xdg-open "https://extensions.gnome.org/extension/19/user-themes/" &>/dev/null)
 
-gnome-extensions enable "apps-menu@gnome-shell-extensions.gcampax.github.com"
+#gnome-extensions enable "apps-menu@gnome-shell-extensions.gcampax.github.com"
 gnome-extensions enable "places-menu@gnome-shell-extensions.gcampax.github.com"
 
 read -p "Press enter to continue... "
